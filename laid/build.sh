7343705383fcecac10f90f20d1be06270e018714
Marc Foley <m.foley.88@gmail.com>
2024-05-14 20:47:44 +0100

    tidy up

--- laid/build.sh
+++ laid/build.sh
@@ -1,3 +1,3 @@
-clang++ -arch arm64 -std=c++17 -I./dependencies/skia/ -L./dependencies/skia/lib   -lskshaper -lskunicode -lskparagraph -lskia -framework CoreServices -framework CoreGraphics -framework CoreText -framework CoreFoundation -framework Metal -framework Foundation -framework QuartzCore $1 pugixml.cpp
+clang++ -arch arm64 -std=c++17 -I./dependencies/skia/ -L./dependencies/skia/lib   -lskshaper -lskunicode -lskparagraph -lskia -framework CoreServices -framework CoreGraphics -framework CoreText -framework CoreFoundation -framework Metal -framework Foundation -framework QuartzCore $1 src/pugixml.cpp
 
 
---
+++ laid/src/xmlparser.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "pugixml.hpp"
+#include "models.h"
+
+namespace laid {
+
+std::shared_ptr<laid::Document> load_file(const char* filename) {
+    pugi::xml_document xml_doc;
+    pugi::xml_parse_result result = xml_doc.load_file(filename);
+    auto xml_doc_start = xml_doc.child("document");
+    auto doc = std::make_shared<laid::Document>();
+
+    // build master pages
+    for (pugi::xml_node node: xml_doc_start.child("head").child("masterPages").children("masterPage")) {
+        laid::MasterPage masterPage;
+        masterPage.name = std::string(node.attribute("name").as_string());
+        masterPage.height = node.attribute("height").as_int();
+        masterPage.width = node.attribute("width").as_int();
+        masterPage.cols = node.attribute("columns").as_int();
+        masterPage.rows = node.attribute("rows").as_int();
+        masterPage.baseline = node.attribute("baseline").as_int();
+
+        doc->addMasterPage(masterPage);
+    }
+
+    // build styles
+    for (pugi::xml_node node: xml_doc_start.child("head").child("styles").children("style")) {
+        laid::Style style;
+        auto nn = node.attribute("name").as_string();
+        style.name = std::string(node.attribute("name").as_string());
+        style.fontName = std::string(node.attribute("fontName").as_string());
+        style.fontSize = node.attribute("fontSize").as_int();
+        style.leading = node.attribute("leading").as_int();
+        doc->addStyle(style);
+    }
+
+    // build pages
+    for (pugi::xml_node node: xml_doc_start.child("body").child("pages").children("page")) {
+        auto masterName = node.attribute("masterPage").as_string();
+        auto page = std::make_shared<laid::Page>(*doc->masterPages[masterName]);
+        page->overflow = node.attribute("overflow").as_bool();
+        std::shared_ptr<laid::Box> prev;
+        bool overflowNext = false;
+        for (pugi::xml_node box_node: node.children("box")) {
+            auto x = box_node.attribute("x").as_int();
+            auto y = box_node.attribute("y").as_int();
+            auto width = box_node.attribute("width").as_int();
+            auto height = box_node.attribute("height").as_int();
+            auto box = std::make_shared<laid::Box>(x, y, width, height);
+            if (overflowNext) {
+                prev->addNext(box);
+            }
+            overflowNext = box_node.attribute("overflowNext").as_bool();
+            for (pugi::xml_node text_node: box_node.children("text")) {
+                auto styleName = text_node.attribute("style").as_string();
+                auto style = doc->paragraph_styles[styleName];
+                auto text = text_node.text().as_string();
+                box->addText(text, style);
+            }
+            page->addBox(box);
+            prev = box;
+        }
+        // TODO add linkedboxes
+        doc->addPage(page);
+    }
+    return doc;
+}
+}
