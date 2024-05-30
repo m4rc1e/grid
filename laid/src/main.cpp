ae4c9d05ea8f04de82fdfcaa3da9e7a43c707a87
Marc Foley <m.foley.88@gmail.com>
2024-05-30 11:12:38 +0100

    start of box flow

--- laid/src/main.cpp
+++ laid/src/main.cpp
@@ -7,14 +7,14 @@
 
 int main(int argc, char *argv[]) {
     // This can be refactored into an argument parser class
-    bool debug = false;
+    bool debug = true;
     for (int i = 0; i < argc; i++) {
         std::cout << "Argument " << i << ": " << argv[i] << std::endl;
         if (std::string(argv[i]) == "--debug") {
             debug = true;
         }
     }
-    auto myDoc = laid::load_file("../sketches/paragraph.xml");
+    auto myDoc = laid::load_file("../sketches/box_flow.xml");
     BuildPDF PDFBuilder(myDoc, "output3.pdf", debug);
     PDFBuilder.Build();
     std::cout << "Done!" << std::endl;
