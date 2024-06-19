1b263cc30e461cd0bd3ea428382f011e66114df8
Marc Foley <m.foley.88@gmail.com>
2024-06-19 11:51:23 +0100

    part implement

--- laid/src/pdfview.h
+++ laid/src/pdfview.h
@@ -49,85 +49,139 @@
 using namespace skia::textlayout;
 
 
-class TextSetter {
-    private:
-        ParagraphBuilderImpl builder;
-        ParagraphBuilderImpl prevBuilder;
+sk_sp<FontCollection> fontCollection = sk_make_sp<FontCollection>();
 
+void setupFontCollection() {
+    fontCollection->setDefaultFontManager(SkFontMgr::RefDefault());
+}
+
+class CursorPos {
     public:
-        TextSetter(
-            int width,
-            int height,
-            skia::textlayout::ParagraphStyle& paragraph_style,
-            sk_sp<FontCollection> fontCollection) : 
-            width(width),
-            height(height),
-            paragraph_style(paragraph_style),
-            fontCollection(fontCollection),
-            builder(paragraph_style, fontCollection),
-            prevBuilder(paragraph_style, fontCollection) {
-        }
-        int width;
-        int height;
-        int contentHeight;
-        bool paintPrev;
-        std::string text;
-        std::string overflow;
-        skia::textlayout::ParagraphStyle paragraph_style;
-        sk_sp<FontCollection> fontCollection;
-        std::unique_ptr<skia::textlayout::Paragraph>  paragraph;
-        std::unique_ptr<skia::textlayout::Paragraph>  prevParagraph;
-        
-        void SetText(
-            std::string text,
-            skia::textlayout::ParagraphStyle paragraph_style
-        ) {
-            auto text_style = paragraph_style.getTextStyle();
-            auto  strut_style = paragraph_style.getStrutStyle();
-
-            std::istringstream ss(text); // " " added at end due to delimiter for std::getline
-            std::string token;
-            
-            while(std::getline(ss, token, ' ')) {
-                builder.pushStyle(text_style);
-                builder.addText(token.data());
-                builder.addText(" ");
-                paragraph = builder.Build();
-                paragraph->layout(width);
-
-                if (paragraph->getHeight() > height) {
-                    paintPrev = true;
-                    overflow += token + " ";
-                    continue;
-                }
-                prevBuilder.pushStyle(text_style);
-                prevBuilder.addText(token.data());
-                prevBuilder.addText(" ");
-                prevParagraph = prevBuilder.Build();
-                prevParagraph->layout(width);
-            };
-        }
-        void Paint(int x, int y, SkCanvas* canvas) {
-            if (paintPrev == true) {
-                if (prevParagraph == nullptr) {
-                    return;
-                }
-                prevParagraph->paint(canvas, x, y);
-                contentHeight = prevParagraph->getHeight();
-                return;
-            }
-            paragraph->paint(canvas, x, y);
-            contentHeight = paragraph->getHeight();
-        }
-        bool HasOverflowingText() {
-            return overflow.size() > 0;
-        }
+    int x;
+    int y;
 };
 
+class TextSetter {
+public:
+    TextSetter(
+        float width,
+        float height,
+        skia::textlayout::ParagraphStyle paragraph_style,
+        std::vector<laid::Box> boxes) : 
+        width(width),
+        height(height),
+        paragraph_style(paragraph_style),
+        boxes(boxes),
+        builder(paragraph_style, fontCollection) {
+    }
+    int width;
+    int height;
+    std::string overflowingText;
+    
+    void SetText(
+        std::string text,
+        skia::textlayout::ParagraphStyle paragraph_style
+    ) {
+        auto text_style = paragraph_style.getTextStyle();
+        builder.pushStyle(text_style);
+
+        std::istringstream ss(text); // " " added at end due to delimiter for std::getline
+        std::string token;
+
+        auto maxLines = int(height / text_style.getFontSize());
+
+        paragraph = builder.Build();
+        while (std::getline(ss, token, ' ')) {
+            auto currentCursor = getCursor(paragraph.get());
+            auto nextCursor = getNextCursor(token, paragraph_style, paragraph.get());
+            auto currentLine = int(paragraph->lineNumber());
+
+            // Handle exceeding height of box
+            if (currentLine >= maxLines -1 && nextCursor.x > width || currentLine >= maxLines) {
+                std::cout << "overflowing" << '\n';
+                overflowingText += token + " ";
+                continue;
+            }
+            // Handle collisions with other Boxes
+            auto collidedBox = boxCollision(nextCursor);
+            if (collidedBox) {
+                addPlaceholder(collidedBox->width - (currentCursor.x - collidedBox->x));
+            }
+
+            builder.addText(token.data());
+            builder.addText(" ");
+            paragraph = builder.Build();
+            paragraph->layout(width);
+
+        }
+    }
+
+    bool HasOverflowingText() {
+        return overflowingText.size() > 0;
+    }
+
+    CursorPos getNextCursor(std::string token, ParagraphStyle style, Paragraph* paragraph) {
+        auto cursor = getCursor(paragraph);
+
+        sk_sp<FontCollection> fontCollection = sk_make_sp<FontCollection>();
+        fontCollection->setDefaultFontManager(SkFontMgr::RefDefault());
+        ParagraphBuilderImpl wordBuilder(style, fontCollection);
+        auto text_style = style.getTextStyle();
+        wordBuilder.pushStyle(text_style);
+
+        wordBuilder.addText(token.data());
+        auto wordParagraph = wordBuilder.Build();
+        wordParagraph->layout(width);
+        auto wordCursor = getCursor(wordParagraph.get());
+        return CursorPos{cursor.x + wordCursor.x, cursor.y};
+    }
+
+    void paint(float x, float y, SkCanvas* canvas) {
+        paragraph->paint(canvas, x, y);
+    }
+
+    CursorPos getCursor(Paragraph* paragraph) {
+        RectHeightStyle rect_height_style = RectHeightStyle::kTight;
+        RectWidthStyle rect_width_style = RectWidthStyle::kTight;
+
+        auto boxes = paragraph->getRectsForRange(0, 1000000, rect_height_style, rect_width_style);
+        int cursorX = boxes.back().rect.fRight;
+        int cursorY = paragraph->getHeight();
+        return CursorPos{cursorX, cursorY};
+    }
+
+    std::optional<laid::Box> boxCollision(CursorPos cursor) {
+        for (auto& box : boxes) {
+            if (
+                cursor.x >= box.x && 
+                cursor.x <= box.x + box.width && 
+                cursor.y >= box.y && 
+                cursor.y <= box.y + box.height
+            ) {
+                return box;
+            }
+        }
+        return std::nullopt;;
+    }
+
+    void addPlaceholder(int width) {
+        PlaceholderStyle placeholder1(width, 0, PlaceholderAlignment::kTop, TextBaseline::kAlphabetic, 0);
+        builder.addPlaceholder(placeholder1);
+    }
+
+private:
+    ParagraphBuilderImpl builder;
+    
+    std::unique_ptr<skia::textlayout::Paragraph>  paragraph;
+    skia::textlayout::ParagraphStyle paragraph_style;
+
+    std::vector<laid::Box>& boxes;
+};
 
 class BuildPDF {
 public:
     BuildPDF(std::shared_ptr<laid::Document> laidDoc, const char* out, bool debug) : laidDoc(laidDoc), stream(out), debug(debug) {
+        setupFontCollection();
         laidDoc = laidDoc;
         fontCollection->setDefaultFontManager(SkFontMgr::RefDefault());
         pdf = SkPDF::MakeDocument(&stream, metadata);
@@ -212,9 +266,11 @@ public:
         BuildStyles();
         BuildPages();
     }
+
     void BuildPages() {
         std::shared_ptr<laid::Page> head = laidDoc->pages;
         while (head != nullptr) {
+            //SolveBoxes(head);
             BuildPage(head);
             head = head->next;
         }
@@ -262,6 +318,7 @@ public:
     void BuildBoxRect(SkCanvas* canvas, std::shared_ptr<laid::Box> box) {
         SkPaint paintBox;
         paintBox.setColor(SK_ColorRED);
+        paintBox.setStrokeWidth(1.0f);
         paintBox.setStyle(SkPaint::kStroke_Style);
 
         SkPaint paintText;
@@ -344,44 +401,70 @@ public:
     }
 
     void BuildText(SkCanvas* canvas, std::shared_ptr<laid::Page> page, std::shared_ptr<laid::Box> box) {
-        int offset = 0;
-        for (size_t paragraph_idx = 0; paragraph_idx < box->paragraphs.size(); paragraph_idx++) {
-            auto paragraph = box->paragraphs[paragraph_idx];
-            auto paraStyle = paragraphStyles[paragraph->style];
-            TextSetter textSetter(box->width, box->height - offset, paraStyle, fontCollection);
 
-            for (size_t textrun_idx = 0; textrun_idx < paragraph->text_runs.size(); textrun_idx++) {
-                auto& text_run = paragraph->text_runs[textrun_idx];
-                textSetter.SetText(text_run.text, paragraphStyles[text_run.style]);
-                if (textSetter.HasOverflowingText()) {
-                    if (page->overflow == true && box->next == nullptr) {
-                        auto newPage = laidDoc->overflowPage(page);
-                    }
-                    if (box->next) {
-                        auto newParagraph = std::make_shared<laid::Paragraph>();
-                        box->next->addParagraph(newParagraph);
-                        newParagraph->addText(textSetter.overflow, text_run.style);
-                        
-                        for (size_t i = paragraph_idx + 1; i < box->paragraphs.size(); i++) {
-                            auto& overflow_paragraph = box->paragraphs[i];
-                            box->next->addParagraph(overflow_paragraph);
-                            for (size_t i = textrun_idx + 1; i < paragraph->text_runs.size(); i++) {
-                                auto& overflow_text_run = paragraph->text_runs[i];
-                                overflow_paragraph->addText(overflow_text_run.text, overflow_text_run.style);
-                            }
-                        }
-                        textSetter.Paint(box->x, box->y, canvas);
-                        return;
-                    } else {
-                        std::cout << "Overflowing text in box!" << std::endl;
-                    }
-                }
-            }
-            textSetter.Paint(box->x, box->y+offset, canvas);
-            // always add a new line when paragraph ends. 
-            // TODO perhaps each box should have a y pos of where content currently is
-            offset += textSetter.contentHeight;
-    }
-};
+        TextStyle text_style;
+        text_style.setFontFamilies({SkString("Inter")});
+        text_style.setColor(SK_ColorBLACK);
+        text_style.setFontSize(14);
+        text_style.setWordSpacing(5);
+        text_style.setLetterSpacing(1);
+        text_style.setDecorationColor(SK_ColorBLACK);
+        text_style.setDecoration(TextDecoration::kUnderline);
+        
+        ParagraphStyle paragraph_style;
+        paragraph_style.turnHintingOff();
+        paragraph_style.setTextStyle(text_style);
+
+        std::vector<laid::Box> boxes = {
+            laid::Box{300, 0, 200, 100},
+            laid::Box{200, 150, 100, 100}
+        };
+        TextSetter textSetter(box->width, box->height, paragraph_style, boxes);
+        textSetter.SetText("On 15 June 1974 the National Front held a march through central London in support of the compulsory repatriation of immigrants. The march was to end at Conway Hall in Red Lion Square. A counter-demonstration was planned by Liberation, an anti-colonial pressure group. During the late 1960s and early 1970s, the London council of Liberation had been increasingly infiltrated by hard-left political activists, and they invited several hard-left organisations to join them in the march. When the Liberation march reached Red Lion Square, the International Marxist Group (IMG) twice charged the police cordon blocking access to Conway Hall. Police reinforcements, including mounted police and units of the Special Patrol Group, forced the rioting demonstrators out of the square. As the ranks of people moved away from the square, Gately was found unconscious on the ground. He was taken to hospital and died later that day. Two further disturbances took place in the vicinity, both involving clashes between the police and the IMG contingent. A public inquiry into the events was conducted by Lord Scarman. He found no evidence that Gately had been killed by the police, as had been alleged by some elements of the hard-left press, and concluded that those who started the riot carry a measure of moral responsibility for his death; and the responsibility is a heavy one.[1] He found fault with some actions of the police on the day. The events in the square made the National Front a household name in the UK, although it is debatable if this had any impact on their share of the vote in subsequent general elections. Although the IMG was heavily criticised by the press and public, there was a rise in localised support and the willingness to demonstrate against the National Front and its policies. There was further violence associated with National Front marches and the counter-demonstrations they faced, including in Birmingham, Manchester, the East End of London (all 1977) and in 1979 in Southall, which led to the death of Blair Peach. After Peach's death, the Labour Party Member of Parliament Syd Bidwell, who had been about to give a speech in Red Lion Square when the violence started, described Peach and Gately as martyrs against fascism and racism.",
+            paragraph_style
+        );
+        textSetter.paint(box->x, box->y, canvas);
+//        int offset = 0;
+//        for (size_t paragraph_idx = 0; paragraph_idx < box->paragraphs.size(); paragraph_idx++) {
+//            auto paragraph = box->paragraphs[paragraph_idx];
+//            auto paraStyle = paragraphStyles[paragraph->style];
+//            std::cout << "style: " << paragraph->style << std::endl;
+//            TextSetter textSetter(box->width, box->height - offset, paraStyle, std::vector<laid::Box>{});
+//
+//            for (size_t textrun_idx = 0; textrun_idx < paragraph->text_runs.size(); textrun_idx++) {
+//                auto& text_run = paragraph->text_runs[textrun_idx];
+//                textSetter.SetText(text_run.text, paragraphStyles[text_run.style]);
+//                if (textSetter.HasOverflowingText()) {
+//                    if (page->overflow == true && box->next == nullptr) {
+//                        auto newPage = laidDoc->overflowPage(page);
+//                    }
+//
+//                        auto newParagraph = std::make_shared<laid::Paragraph>();
+//                        newParagraph->style = paragraph->style;
+//                        box->next->addParagraph(newParagraph);
+//                        std::cout << "text_run_style: " << text_run.style << std::endl;
+//                        newParagraph->addText(textSetter.overflowingText, text_run.style);
+//                        
+//                        for (size_t i = paragraph_idx + 1; i < box->paragraphs.size(); i++) {
+//                            auto& overflow_paragraph = box->paragraphs[i];
+//                            box->next->addParagraph(overflow_paragraph);
+//                            for (size_t i = textrun_idx + 1; i < paragraph->text_runs.size(); i++) {
+//                                auto& overflow_text_run = paragraph->text_runs[i];
+//                                std::cout << "overflowing text run: " << overflow_text_run.style << std::endl;
+//                                overflow_paragraph->addText(overflow_text_run.text, overflow_text_run.style);
+//                            }
+//                        }
+//                        textSetter.paint(box->x, box->y, canvas);
+//                    } else {
+//                        std::cout << "Overflowing text in box!" << std::endl;
+//                    }
+//                }
+//            }
+//            textSetter.Paint(box->x, box->y+offset, canvas);
+//            // always add a new line when paragraph ends. 
+//            // TODO perhaps each box should have a y pos of where content currently is
+//            offset += textSetter.contentHeight;
+//        }
+    };
 };
 #endif
